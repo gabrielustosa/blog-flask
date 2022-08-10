71ff3ed9bcc426633e665c3b64f5b645f20cb5aa
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-08-10 18:29:07 -0300

    FEAT[LOGIN]: Aprendo login e register

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Password Confirmation', validators=[DataRequired(), EqualTo('password', message='The password must be equal.')])
+    confirm_password = PasswordField('Password Confirmation', validators=[DataRequired(), EqualTo('password',
+                                                                                                  message='The password must be equal.')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+
+        if user:
+            raise ValidationError('That username is taken, Please choose another username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+
+        if user:
+            raise ValidationError('That email is taken, Please choose another email')
 
 
 class LoginForm(FlaskForm):
